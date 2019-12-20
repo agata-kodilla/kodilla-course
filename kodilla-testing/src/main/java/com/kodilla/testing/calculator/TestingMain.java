cd0ced44ae59da2f6102fc348d51c696f57c071a
abialkowska <abialkowska247@gmail.com>
2019-12-20 17:15:33 +0100

    add first unit test - calculator

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
@@ -0,0 +1,20 @@
+package main.java.com.kodilla.testing.calculator;
+
+public class TestingMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int add = calculator.add(6, 8);
+        int substract = calculator.substract(23, 455);
+
+        if(add==14){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error! for add");
+        }
+        if(substract==-432){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error! for substract");
+        }
+    }
+}
