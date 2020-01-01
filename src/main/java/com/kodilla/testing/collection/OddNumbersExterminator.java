cf72164f937a2866efbc07e284a017830e4e3d4d
abialkowska <abialkowska247@gmail.com>
2020-01-01 15:39:38 +0100

    collection - delete assignment

--- src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumbersExterminator {
-    
+
     public List<Integer> exterminate(List<Integer> list){
         ArrayList<Integer> integers = new ArrayList<>(list);
         integers.removeIf(i -> i % 2 != 0);
-        list = integers;
-        return list;
+        return integers;
     }
 }
