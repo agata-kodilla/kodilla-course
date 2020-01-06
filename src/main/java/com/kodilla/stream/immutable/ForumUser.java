c21567a0c27b3c5ac11259b84d157ed8e8d6b3bb
abialkowska <abialkowska247@gmail.com>
2020-01-06 21:29:37 +0100

    add ForumUser

---
+++ src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,27 @@
+package com.kodilla.stream.immutable;
+
+public final class ForumUser {
+    private String username;
+    private String realName;
+
+    public ForumUser(String username, String realName) {
+        this.username = username;
+        this.realName = realName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+}
