fa6b73e8b15fa4ca22e8d3faf6a4cdba7e6f6622
abialkowska <abialkowska247@gmail.com>
2020-01-06 16:05:49 +0100

    add classes and tests for book library

---
+++ src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -0,0 +1,31 @@
+package com.kodilla.testing.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookLibrary {
+    LibraryDatabase libraryDatabase;
+
+    public BookLibrary(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+    }
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        List<Book> bookList = new ArrayList<Book>();
+        if (titleFragment.length() < 3) return bookList;
+        List<Book> resultList = libraryDatabase
+                .listBooksWithCondition(titleFragment);
+        if (resultList.size() > 20) return bookList;
+        bookList = resultList;
+        return bookList;
+    }
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser){
+        List<Book> bookListInHands;
+        if (libraryUser == null) {
+            System.out.println("There is no library's user in db");
+        }
+        bookListInHands = libraryDatabase.listBooksInHandsOf(libraryUser);
+        return bookListInHands;
+    }
+}
