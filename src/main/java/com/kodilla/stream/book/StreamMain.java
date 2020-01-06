e4a3ac70fddcb8ba75cebd7a764753e52f71157b
abialkowska <abialkowska247@gmail.com>
2020-01-06 22:25:20 +0100

    add classes with exercises

---
+++ src/main/java/com/kodilla/stream/book/StreamMain.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.book;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
+
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+    }
+}
